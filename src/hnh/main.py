a862ef0b141212ab3b13d68962079d6086dc382a
Seokxkyu <kyuseok000@gmail.com>
2024-09-26 18:08:29 +0900

    update main

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,19 +1,15 @@
-from fastapi import FastAPI, UploadFile, Request
+from fastapi import FastAPI, File, UploadFile
 from fastapi.templating import Jinja2Templates
-from transformers import pipeline
+from fastapi import Request
 from typing import Union
-from PIL import Image
-import random
+from transformers import pipeline
+from hnh.utils import get_max_label
 import os
-import io
+import random
 
 app = FastAPI()
 
 html = Jinja2Templates(directory="public")
-
-@app.get("/hello")
-def read_root():
-    return {"Hello": "hotdog"}
 
 @app.get("/")
 async def home(request: Request):
@@ -24,14 +20,25 @@ async def home(request: Request):
 
 @app.get("/predict")
 def hotdog():
-    model = pipeline("image-classification", model="julien-c/hotdog-not-hotdog") 
-    return {"Hello": random.choice(["hotdog", "not hotdog"])}
+    pre = ("Not Hotdog", "Hotdog")
+    return random.choice(pre)
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    # 파일 저장
     img = await file.read()
     model = pipeline("image-classification", model="julien-c/hotdog-not-hotdog")
-    img = Image.open(io.BytesIO(img)) # img 바이트를 PIL 이미지로 변환
-    predictions = model(img)
 
-    return {"Hello": predictions}
+    from PIL import Image
+    import io
+    img = Image.open(io.BytesIO(img)) # 이미지 byte를 PIL 이미지로 변환
+    p = model(img)
+    # {'label': 'hot dog', 'score': 0.54}
+
+    # if p 값이 배열과 같이 나오면 높은 확률의 값을 추출해서 리턴하기
+    score = p[0]['score']
+    if score >= 0.8:
+        return {"prediction": p, "label": "hot dog"}
+    else:
+        return {"label": "not hot dog"}
